fa5b6db211ae657a08d0301c81b7f22822220a0a
Deivid <david_recheni@hotmail.com>
2022-04-28 19:16:00 +0200

    Github stats switch on dark mode

--- components/CV/GithubStats.tsx
+++ components/CV/GithubStats.tsx
@@ -1,17 +1,25 @@
 import Image from "next/image";
 import data from "../../data/CVData";
+import { useTheme } from "next-themes";
 
 const statsUrlDark = `https://github-readme-stats.vercel.app/api?username=${data.githubUsername}&count_private=true&hide_border=true&show_icons=true&hide_title=true&hide=contribs&theme=github_dark`;
 const statsUrlLight = `https://github-readme-stats.vercel.app/api?username=${data.githubUsername}&count_private=true&hide_border=true&show_icons=true&hide_title=true&hide=contribs`;
-const GithubStats = () => (
-  <div className="my-2 w-full flex justify-center">
-    <div className="dark:hidden">
-      <Image height={200} width={550} src={statsUrlLight} alt="Github stats" />
+const GithubStats = () => {
+  const { theme } = useTheme();
+  return (
+    <div className="my-2 w-full flex justify-center">
+      {theme === "dark" ? (
+        <Image height={200} width={550} src={statsUrlDark} alt="Github stats" />
+      ) : (
+        <Image
+          height={200}
+          width={550}
+          src={statsUrlLight}
+          alt="Github stats"
+        />
+      )}
     </div>
-    <div className="hidden dark:block">
-      <Image height={200} width={550} src={statsUrlDark} alt="Github stats" />
-    </div>
-  </div>
-);
+  );
+};
 
 export default GithubStats;
