4c8617889646588c883fb1a48d7546a73861901e
Deivid <david_recheni@hotmail.com>
2022-04-30 09:49:39 +0200

    Responsiveness improvement

--- components/CV/AboutContainer.tsx
+++ components/CV/AboutContainer.tsx
@@ -5,8 +5,8 @@ import data from "../../data/CVData";
 const AboutContainer = () => (
   <div className="flex flex-col">
     <Header>About me</Header>
-    <div className="flex justify-start">
-      <p className="max-w-prose text-justify text-sm sm:text-base">
+    <div className="flex justify-center px-2">
+      <p className="max-w-fit text-justify text-sm sm:text-base">
         {data.about}
       </p>
     </div>
