5ce3cb0cbca27146714c403e02778c62720790a3
Deivid <david_recheni@hotmail.com>
2022-06-14 19:18:07 +0200

    No print screen

--- components/CV/CVContainer/TopRow.tsx
+++ components/CV/CVContainer/TopRow.tsx
@@ -1,18 +1,11 @@
-import ToggleDarkMode from "./TopRow/ToggleDarkMode";
 import NameAndRole from "./TopRow/NameAndRole";
-import { BsPrinterFill } from "react-icons/bs";
-import Link from "next/link";
+import ToggleDarkMode from "./TopRow/ToggleDarkMode";
 
 const TopRow = () => {
   return (
     <div className="flex justify-between items-center">
       <NameAndRole />
-      <div className="flex gap-2">
-        <Link href="/print">
-          <a>
-            <BsPrinterFill size={32} />
-          </a>
-        </Link>
+      <div className="flex">
         <ToggleDarkMode />
       </div>
     </div>
