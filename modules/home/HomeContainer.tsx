88de1f13e4c30af5651095ead76a174c141a47c3
Deivid <david_recheni@hotmail.com>
2022-06-21 23:39:39 +0200

    404 fix

--- modules/home/HomeContainer.tsx
+++ modules/home/HomeContainer.tsx
@@ -27,8 +27,7 @@ const HomeContainer = () => {
       <Canvas>
         <ambientLight />
         <pointLight position={[10, 10, 10]} />
-        <Box position={[-1.2, 0, 0]} />
-        <Box position={[1.2, 0, 0]} />
+        <Box position={[0, 0, 0]} />
         <DavidSculpture />
       </Canvas>
     </div>
--- modules/home/components/DavidSculpture.tsx
+++ modules/home/components/DavidSculpture.tsx
@@ -1,10 +1,8 @@
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { useLoader } from "@react-three/fiber";
-import sculpture from "../assets/DavidSculpture/source/scene.gltf";
 const DavidSculpture = () => {
-  const obj = useLoader(GLTFLoader, sculpture);
-
-  return obj && <primitive object={obj} />;
+  const obj = useLoader(GLTFLoader, "./Model/scene.gltf");
+  return obj && <primitive object={obj} position={[0, 0, 0]} />;
 };
 
 export default DavidSculpture;
