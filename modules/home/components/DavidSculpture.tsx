b61eefd70b580baa6551bb9c85a0c3a611831965
Deivid <david_recheni@hotmail.com>
2022-07-16 09:57:49 +0200

    Added movement

--- modules/home/components/DavidSculpture.tsx
+++ modules/home/components/DavidSculpture.tsx
@@ -1,6 +1,7 @@
+import { useFrame, useLoader, Vector3 } from "@react-three/fiber";
+import { useRef } from "react";
+import { Mesh, TextureLoader } from "three";
 import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
-import { useLoader, Vector3 } from "@react-three/fiber";
-import { TextureLoader } from "three";
 
 interface Props {
   scale?: number;
@@ -13,10 +14,22 @@ const DavidSculpture = ({ scale, position }: Props) => {
   ]);
   const obj = useLoader(OBJLoader, "./assets/DavidSculpture/rapid.obj");
 
+  const ref = useRef<Mesh>(null);
+
+  useFrame((_) => {
+    if (ref?.current?.position)
+      return (ref.current.position.y =
+        -4.4 + Math.sin(_.clock.elapsedTime) / 8);
+  });
+
   return (
-    <mesh position={position} scale={scale} castShadow receiveShadow>
+    <mesh position={position} scale={scale} castShadow receiveShadow ref={ref}>
       <primitive object={obj} />;
-      <meshStandardMaterial attach="material" map={texture} />
+      <meshStandardMaterial
+        attach="material"
+        map={texture}
+        alphaMap={occlusion}
+      />
     </mesh>
   );
 };
