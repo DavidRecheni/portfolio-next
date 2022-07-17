33c37df81bf2a2fd5d85e5c6e393907018b3c1b9
Deivid <david_recheni@hotmail.com>
2022-07-17 20:53:23 +0200

    Tablet responsive

--- modules/home/HomeContainer.tsx
+++ modules/home/HomeContainer.tsx
@@ -4,7 +4,7 @@ import SculptureScene from "./components/SculptureScene";
 const HomeContainer = () => {
   return (
     <div className="relative h-screen w-screen flex flex-col items-center text-white ">
-      <div className="absolute left-48 h-screen flex items-center">
+      <div className="absolute right-6 2xl:left-48 h-screen flex items-center">
         <div className="flex flex-col">
           <h1 className="font-['Abhaya\\Libre']  text-8xl">Hola.</h1>
           <h2 className="text-2xl mt-2">David Recheni</h2>
@@ -16,13 +16,13 @@ const HomeContainer = () => {
       </div>
 
       <div className="flex w-full h-screen relative">
-        <div className="absolute w-full h-screen -top-20">
+        <div className="absolute w-full h-screen xl:-top-44 -top-20">
           <SculptureScene />
         </div>
 
-        <div className="absolute h-screen right-48 flex flex-col justify-center">
+        <div className="absolute h-screen right-14 2xl:right-48 flex flex-col justify-end 2xl:justify-center">
           <Link href="/cv">
-            <h2 className="mt-20 mr-20 hover:cursor-pointer border-b-2 px-1 pb-1 border-slate-400 hover:border-white border- text-3xl text-white">
+            <h2 className="mt-20 mr-20 2xl:mb-0 mb-20 hover:cursor-pointer border-b-2 px-1 pb-1 border-slate-400 hover:border-white border- text-3xl text-white">
               CV
             </h2>
           </Link>
