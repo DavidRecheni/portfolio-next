70c4b4227d1c8df8c09d6ea6789b41884d4ff5d3
Deivid <david_recheni@hotmail.com>
2022-07-17 19:41:37 +0200

    Back button

--- modules/CV/CVContainer.tsx
+++ modules/CV/CVContainer.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+import { BsFillArrowLeftSquareFill } from "react-icons/bs";
 import LeftColumn from "./components/elements/LeftColumn";
 import RightColumn from "./components/elements/RightColumn";
 import TopRow from "./components/elements/TopRow";
@@ -5,6 +7,11 @@ import TopRow from "./components/elements/TopRow";
 const CVContainer = () => {
   return (
     <div className="max-w-4xl my-2 sm:my-8 px-4 dark:text-github-gray">
+      <div className="absolute top-10 left-10">
+        <Link href="/">
+          <BsFillArrowLeftSquareFill className="text-5xl print:hidden hover:text-slate-600 active:text-slate-500" />
+        </Link>
+      </div>
       <TopRow />
 
       <div className="border-b-4 border-black dark:border-white my-4" />
