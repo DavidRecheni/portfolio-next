288533a04cd007065c06529759dc7730c459bd17
David <davidrecheni@gmail.com>
2024-05-13 11:46:57 +0200

    Improve laptop responsive

--- modules/home/HomeContainer.tsx
+++ modules/home/HomeContainer.tsx
@@ -11,13 +11,13 @@ const HomeContainer = () => {
       <SocialMedia />
       <div className="h-full w-screen flex items-center justify-between px-2 sm:px-16 xl:px-36">
         <div className="flex flex-col">
-          <div className="flex flex-col text-[100px] md:text-[150px] xl:text-[200px] antialiased">
-            <h1 className="font-['Abhaya\\Libre'] md:-mb-10 xl:-mb-20 ">
+          <div className="flex flex-col text-[100px] md:text-[150px] 2xl:text-[200px] antialiased">
+            <h1 className="font-['Abhaya\\Libre'] md:-mb-10 2xl:-mb-20 ">
               David
             </h1>
             <h1 className="font-['Abhaya\\Libre'] -mt-16 -mb-8">Recheni</h1>
           </div>
-          <div className="text-4xl xl:text-5xl text-slate-300 font-light">
+          <div className="text-4xl 2xl:text-5xl text-slate-300 font-light">
             <h2>Full-stack TS Developer</h2>
             <h2>Interaction designer</h2>
             <h2>Entrepreneur</h2>
