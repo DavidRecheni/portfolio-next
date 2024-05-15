bbe9ff1729b7c2ec9d2e6ed0e1b2c16aba83301a
David Recheni <davidrecheni@gmail.com>
2024-05-15 09:27:10 +0200

    Make social media clickable

--- modules/home/components/SocialMedia/SocialMedia.tsx
+++ modules/home/components/SocialMedia/SocialMedia.tsx
@@ -21,7 +21,7 @@ const links = [
 
 const SocialMedia = () => {
   return (
-    <div className="absolute top-0 left-0 right-0 ml-auto mr-auto w-44">
+    <div className="absolute top-0 left-0 right-0 ml-auto mr-auto w-44 z-10">
       <div className="flex items-center justify-center gap-5 py-2 rounded-b-xl bg-white text-gray-900">
         {links.map((link) => (
           <IconLink link={link} key={link.to} />
